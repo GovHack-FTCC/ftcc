ca47468713664634d91c23e2702b57f779fd5883
Ryan Macalandag <rmacalandag@gmail.com>
2024-03-02 00:32:49 +1100

    fixed scroll, added quotes

--- script.js
+++ script.js
@@ -3,18 +3,12 @@ const header = document.querySelector('header');
 
 window.addEventListener('scroll', (e) => {
   const scrollHeight = window.scrollY;
-
-  console.log(scrollHeight)
-  
-  if (scrollHeight > 400) {
-    document.body.classList.add('scrolling');
-  } else if (scrollHeight <= 400) {
-    document.body.classList.remove('scrolling');
+  if (scrollHeight > 0) {
+    header.classList.add('folded');
   }
 });
 
 downButton.addEventListener('click', (e) => {
-  console.log('hey')
   header.classList.toggle('folded');
   window.scrollTo({ top: 0, behavior: 'smooth' });
 })
