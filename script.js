95c9f6ded572b4cb3dc47be94bec39870eea546a
Ryan Macalandag <rmacalandag@gmail.com>
2024-03-01 18:03:49 +1100

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const downButton = document.querySelector('header .button');
+const header = document.querySelector('header');
+
+window.addEventListener('scroll', (e) => {
+  const scrollHeight = window.scrollY;
+
+  console.log(scrollHeight)
+  
+  if (scrollHeight > 400) {
+    document.body.classList.add('scrolling');
+  } else if (scrollHeight <= 400) {
+    document.body.classList.remove('scrolling');
+  }
+});
+
+downButton.addEventListener('click', (e) => {
+  console.log('hey')
+  header.classList.toggle('folded');
+})
